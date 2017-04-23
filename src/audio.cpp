a9eccb2bbc68a7b18720cd22a4756cbbf7144565
Max Jöhnk <maxjoehnk@gmail.com>
2017-04-24 00:13:44 +0200

    remove debug messages

--- src/audio.cpp
+++ src/audio.cpp
@@ -63,25 +63,14 @@ message_handler (GstBus * bus, GstMessage * message, gpointer data)
       /* we can get the number of channels as the length of any of the value
        * arrays */
       channels = rms_arr->n_values;
-      g_print ("endtime: %" GST_TIME_FORMAT ", channels: %d\n",
           GST_TIME_ARGS (endtime), channels);
       for (i = 0; i < channels; ++i) {
 
-        g_print ("channel %d\n", i);
         value = g_value_array_get_nth (rms_arr, i);
         rms_dB = g_value_get_double (value);
 
-        value = g_value_array_get_nth (peak_arr, i);
-        peak_dB = g_value_get_double (value);
-
-        value = g_value_array_get_nth (decay_arr, i);
-        decay_dB = g_value_get_double (value);
-        g_print ("    RMS: %f dB, peak: %f dB, decay: %f dB\n",
-            rms_dB, peak_dB, decay_dB);
-
         /* converting from dB to normal gives us a value between 0.0 and 1.0 */
         rms = pow (10, rms_dB / 20);
-        g_print ("    normalized rms value: %f\n", rms);
 
         visualize_rms(i, rms);
       }
