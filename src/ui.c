a7286519727c8c675ffa0aaf209009cd7bcd6edf
Max Jöhnk <maxjoehnk@gmail.com>
2017-04-23 20:44:26 +0200

    fix errors and add button alert function

--- src/ui.c
+++ src/ui.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <pigpio.h>
 
 const int RECORD_BUTTON = 26;
@@ -7,12 +8,17 @@ const int GREEN_2 = 6;
 const int YELLOW = 5;
 const int RED = 0;
 
+static void _cb(int gpio, int level, uint32_t tick, void *user) {
+    printf("Button pressed");
+}
+
 int main(int argc, char** argv) {
     if (gpioInitialise() < 0) {
         return 1;
     }
-
     gpioSetMode(RECORD_BUTTON, PI_INPUT);
+    gpioSetPullUpDown(RECORD_BUTTON, PI_PUD_UP);
+    gpioSetAlertFunc(RECORD_BUTTON, _cb);
     gpioSetMode(RECORD_LED, PI_OUTPUT);
     gpioSetMode(GREEN_1, PI_OUTPUT);
     gpioSetMode(GREEN_2, PI_OUTPUT);
@@ -23,6 +29,9 @@ int main(int argc, char** argv) {
     gpioWrite(GREEN_2, 1);
     gpioWrite(YELLOW, 1);
     gpioWrite(RED, 1);
-    
-    return gpioTerminate();
+
+    sleep(300);
+
+    gpioTerminate();
+    return 0;
 }
