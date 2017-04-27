f6427f3a20add2553e1687e919c3f1f690995b20
Max Jöhnk <maxjoehnk@gmail.com>
2017-04-27 14:28:31 +0200

    add initial python code

---
+++ src/recordingpi.py
@@ -0,0 +1,47 @@
+import Rpi.GPIO as GPIO
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+import Adafruit_MCP230xx.MCP23008 as MCP23008
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Setup GPIO
+RECORD_BUTTON_PIN = 5;
+RECORD_LED_PIN = 6;
+
+GPIO.setmode(GPIO.BCM);
+
+GPIO.setup(RECORD_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP);
+GPIO.setup(RECORD_LED_PIN, GPIO.OUT);
+
+# Setup Display
+DISPLAY_RST_PIN = 24;
+DISPLAY_DC_PIN = 23;
+DISPLAY_SPI_PORT = 0;
+DISPLAY_SPI_DEVICE = 0;
+
+display = Adafruit_SSD1306.SSD1306_128_32(rst=DISPLAY_RST_PIN, dc=DISPLAY_DC_PIN, spi=SPI.SpiDev(DISPLAY_SPI_PORT, DISPLAY_SPI_DEVICE, max_speed_hz=8000000));
+
+# Initialize Display
+display.begin();
+
+# Clear Display
+display.clear();
+display.display();
+
+width = 128;
+height = 32;
+
+image = Image.new('1', (width, height));
+
+draw = ImageDraw.Draw(image);
+draw.rectangle((0, 0, width, height), outline=0, fill=0);
+
+disp.image(image);
+disp.display();
+
+except KeyboardInterrupt:
+    GPIO.cleanup();
