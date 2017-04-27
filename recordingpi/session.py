9c68551ca3c5dd6811c592639f36f3666cbda126
Max Jöhnk <maxjoehnk@gmail.com>
2017-04-27 17:32:37 +0200

    add placeholder methods

--- recordingpi/session.py
+++ recordingpi/session.py
@@ -3,3 +3,12 @@ from datetime import datetime
 class Session:
     def __init__(self, date=datetime.now()):
         self.date = date;
+
+    def record():
+
+    def play():
+
+    def delete():
+
+    def close():
+        
