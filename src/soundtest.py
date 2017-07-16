5b62579f07047bc79900077560d796aa7f77b979
Max Jöhnk <maxjoehnk@gmail.com>
2017-07-16 18:27:08 +0200

    [host] gstreamer prototype now records stereo and mono files

--- src/soundtest.py
+++ src/soundtest.py
@@ -17,8 +17,8 @@ def on_message(bus, message):
         struct = Gst.Message.get_structure(message)
         if struct.get_name() == 'level':
             array_val = struct.get_value('rms')
-            for value in array_val:
-                print pow(10, value / 20)
+            for idx, value in enumerate(array_val):
+                print '%(id)d: %(level)f' % {'id': idx, 'level': pow(10, value / 20)}
 
 def on_pad_added(element, pad):
     sink = None;
@@ -27,6 +27,14 @@ def on_pad_added(element, pad):
         sink = recorder0.get_static_pad('sink_0')
     elif pad.get_name() == 'src_1':
         sink = recorder0.get_static_pad('sink_1')
+    elif pad.get_name() == 'src_2':
+        pipeline.add(recorder1)
+        sink = recorder1.get_static_pad('sink_0')
+    elif pad.get_name() == 'src_3':
+        pipeline.add(recorder2)
+        sink = recorder2.get_static_pad('sink_0')
+    else:
+        return
     pad.link(sink)
 
 def new_level_bin():
@@ -49,31 +57,43 @@ def new_level_bin():
 def new_recorder_bin(path, sinks=1):
     bin = Gst.Bin()
     interleave = Gst.ElementFactory.make('interleave', None)
-    queue = Gst.ElementFactory.make('queue', None)
     encoder = Gst.ElementFactory.make('wavenc', None)
     output = Gst.ElementFactory.make('filesink', None)
     output.set_property('location', path)
     bin.add(interleave)
-    bin.add(queue)
     bin.add(encoder)
     bin.add(output)
-    interleave.link(queue)
-    queue.link(encoder)
+    interleave.link(encoder)
     encoder.link(output)
     if sinks == 1:
+        queue = Gst.ElementFactory.make('queue', None)
+        bin.add(queue)
         interleave.set_property('channel_positions', [GstAudio.AudioChannelPosition.MONO])
         sink = interleave.get_request_pad('sink_0')
-        ghostpad = Gst.GhostPad.new('sink_0', sink)
+        queueSink = queue.get_static_pad('sink')
+        queueSrc = queue.get_static_pad('src')
+        queueSrc.link(sink)
+        ghostpad = Gst.GhostPad.new('sink_0', queueSink)
         bin.add_pad(ghostpad)
     elif sinks == 2:
+        queue0 = Gst.ElementFactory.make('queue', 'Queue L')
+        queue1 = Gst.ElementFactory.make('queue', 'Queue R')
+        bin.add(queue0)
+        bin.add(queue1)
         interleave.set_property('channel_positions', [
             GstAudio.AudioChannelPosition.FRONT_LEFT,
             GstAudio.AudioChannelPosition.FRONT_RIGHT
         ])
         sink0 = interleave.get_request_pad('sink_0')
-        ghostpad0 = Gst.GhostPad.new('sink_0', sink0)
+        queueSink0 = queue0.get_static_pad('sink')
+        queueSrc0 = queue0.get_static_pad('src')
+        queueSrc0.link(sink0)
+        ghostpad0 = Gst.GhostPad.new('sink_0', queueSink0)
         sink1 = interleave.get_request_pad('sink_1')
-        ghostpad1 = Gst.GhostPad.new('sink_1', sink1)
+        queueSink1 = queue1.get_static_pad('sink')
+        queueSrc1 = queue1.get_static_pad('src')
+        queueSrc1.link(sink1)
+        ghostpad1 = Gst.GhostPad.new('sink_1', queueSink1)
         bin.add_pad(ghostpad0)
         bin.add_pad(ghostpad1)
     return bin
@@ -89,8 +109,8 @@ recorder = Gst.ElementFactory.make('queue', 'Recording Queue')
 deinterleave = Gst.ElementFactory.make('deinterleave', 'Splitter')
 
 recorder0 = new_recorder_bin('/Users/max/Documents/Code/recording-pi/test.wav', 2)
-recorder1 = new_recorder_bin('/Users/max/Documents/Code/recording-pi/test-1.wav')
-recorder2 = new_recorder_bin('/Users/max/Documents/Code/recording-pi/test-2.wav')
+recorder1 = new_recorder_bin('/Users/max/Documents/Code/recording-pi/test-1.wav', 1)
+recorder2 = new_recorder_bin('/Users/max/Documents/Code/recording-pi/test-2.wav', 1)
 
 pipeline.add(src)
 pipeline.add(convert)
@@ -99,8 +119,6 @@ pipeline.add(level)
 pipeline.add(recorder)
 pipeline.add(deinterleave)
 pipeline.add(recorder0)
-#pipeline.add(recorder1)
-#pipeline.add(recorder2)
 
 src.link(convert)
 convert.link(tee)
