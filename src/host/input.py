f64de727588a204e6ada4c44ff49377f7273fad6
Max Jöhnk <maxjoehnk@gmail.com>
2017-07-17 23:51:37 +0200

    [host] remove remaining unused gpio code

--- src/host/input.py
+++ src/host/input.py
@@ -8,26 +8,15 @@ global ENCODER_CLK_LAST_STATE
 global ENCODER_BTN_STATE
 global ENCODER_BTN_LAST_STATE
 
-global RECORD_BTN_STATE
-global RECORD_BTN_LAST_STATE
-
 def capture():
     # Persist old state
     ENCODER_CLK_LAST_STATE = ENCODER_CLK_STATE
     ENCODER_BTN_LAST_STATE = ENCODER_BTN_STATE
-    RECORD_BTN_LAST_STATE = RECORD_BTN_STATE
 
     # Fetch new State
     ENCODER_CLK_STATE = GPIO.input(ENCODER_CLK_PIN)
     ENCODER_DT_STATE = GPIO.input(ENCODER_DT_PIN)
     ENCODER_BTN_STATE = GPIO.input(ENCODER_BTN_STATE)
-    RECORD_BTN_STATE = GPIO.input(RECORD_BUTTON_PIN)
-
-def isRecordBtnPressed():
-    if RECORD_BTN_STATE != RECORD_BTN_LAST_STATE:
-        if RECORD_BTN_STATE == 1:
-            return True
-    return False
 
 def isEncoderBtnPressed():
     if ENCODER_BTN_STATE != ENCODER_BTN_LAST_STATE:
