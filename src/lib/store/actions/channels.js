f4f2f92ed45276f599668a6949c289e53aac247d
Max Jöhnk <maxjoehnk@gmail.com>
2017-07-17 18:57:30 +0200

    [node] add (un)linking and (dis)arming of channels

--- src/lib/store/actions/channels.js
+++ src/lib/store/actions/channels.js
@@ -1,11 +1,43 @@
 const CHANNELS_AUDIO_LEVEL = '[Channel] Audio Level';
+const ARM_CHANNEL = '[Channel] Arm';
+const DISARM_CHANNEL = '[Channel] Disarm';
+const LINK_CHANNELS = '[Channel] Link';
+const UNLINK_CHANNELS = '[Channel] Unlink';
 
 const audioLevel = level => ({
     type: CHANNELS_AUDIO_LEVEL,
     payload: level
 });
 
+const armChannel = index => ({
+    type: ARM_CHANNEL,
+    payload: index
+});
+
+const disarmChannel = index => ({
+    type: DISARM_CHANNEL,
+    payload: index
+});
+
+const linkChannels = channels => ({
+    type: LINK_CHANNELS,
+    payload: channels
+});
+
+const unlinkChannels = channels => ({
+    type: UNLINK_CHANNELS,
+    payload: channels
+});
+
 module.exports = {
     CHANNELS_AUDIO_LEVEL,
-    audioLevel
+    ARM_CHANNEL,
+    DISARM_CHANNEL,
+    LINK_CHANNELS,
+    UNLINK_CHANNELS,
+    audioLevel,
+    armChannel,
+    disarmChannel,
+    linkChannels,
+    unlinkChannels
 };
--- src/lib/store/reducers/recording.js
+++ src/lib/store/reducers/recording.js
@@ -1,12 +1,26 @@
 const { START_RECORDING, STOP_RECORDING } = require('../actions/recording');
 const { OPEN_SESSION_SUCCESS } = require('../actions/session');
+const { ARM_CHANNEL, DISARM_CHANNEL, LINK_CHANNELS, UNLINK_CHANNELS } = require('../actions/channels');
 
 const initialState = {
     active: false,
     files: [],
-    channels: [
-        [1, 2]
-    ]
+    channels: {
+        armed: [
+            true,
+            false,
+            false,
+            false,
+            false
+        ],
+        setup: [
+            [1, 2],
+            [3],
+            [4],
+            [5],
+            [6]
+        ]
+    }
 }
 
 module.exports = (state = initialState, action) => {
@@ -15,12 +29,15 @@ module.exports = (state = initialState, action) => {
             return {
                 active: false,
                 files: [],
-                channels: action.payload.channels
+                channels: action.payload.channels // TODO: remap
             };
         case START_RECORDING:
+            const channels = state.channels.setup.filter((channels, i) => {
+                return state.channels.armed[i];
+            });
             return Object.assign({}, state, {
                 active: true,
-                files: state.channels.map((channels, i) => {
+                files: channels.map((channels, i) => {
                     return {
                         filename: `${action.payload}-${i}.wav`,
                         channels: channels
@@ -32,6 +49,72 @@ module.exports = (state = initialState, action) => {
                 active: false,
                 files: []
             });
+        case ARM_CHANNEL: { // Should we use the index here?
+            const armed = state.channels.armed.map((value, i) => {
+                if (i === action.payload) {
+                    return true;
+                }
+                return value;
+            });
+            return Object.assign({}, state, {
+                channels: {
+                    armed,
+                    setup: state.channels.setup
+                }
+            });
+        }
+        case DISARM_CHANNEL: { // Should we use the index here?
+            const armed = state.channels.armed.map((value, i) => {
+                if (i === action.payload) {
+                    return false;
+                }
+                return value;
+            });
+            return Object.assign({}, state, {
+                channels: {
+                    armed,
+                    setup: state.channels.setup
+                }
+            });
+        }
+        case LINK_CHANNELS: { // @TODO: update armed array too
+            const setup = [
+                ...state.channels.setup.filter((channels) =>
+                    !channels.includes(action.payload[0]) && !channels.includes(action.payload[1])),
+                action.payload
+            ].sort((a, b) => {
+                if (a[0] < b[0]) {
+                    return -1;
+                }else {
+                    return 1;
+                }
+            });
+            return Object.assign({}, state, {
+                channels: {
+                    armed: state.channels.armed,
+                    setup: setup
+                }
+            });
+        }
+        case UNLINK_CHANNELS: { // @TODO: update armed array too
+            const setup = [
+                ...state.channels.setup.filter((channels) => JSON.stringify(channels) !== JSON.stringify(action.payload)),
+                [action.payload[0]],
+                [action.payload[1]]
+            ].sort((a, b) => {
+                if (a[0] < b[0]) {
+                    return -1;
+                }else {
+                    return 1;
+                }
+            });
+            return Object.assign({}, state, {
+                channels: {
+                    armed: state.channels.armed,
+                    setup: setup
+                }
+            });
+        }
         default:
             return state;
     }
