404f9d3ad5b76b0df55102b9bd57226714858eee
Max Jöhnk <maxjoehnk@gmail.com>
2017-07-17 19:25:56 +0200

    [node] add port configuration to config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,5 +4,42 @@ module.exports = {
         clientApi: 3000,
         hostApi: 4000,
         python: 5000
+    },
+    gpio: {
+        // Free Ports: 6, 7
+        // Ports 8-16 are special function ports
+        // Ports 4-5 are used for the Display
+        // Ports 18-20 are used for the Encoder
+        leds: {
+            channels: {
+                1: 100,
+                2: 104,
+                3: 200,
+                4: 204,
+                5: 300,
+                6: 304
+            },
+            links: {
+                12: 0,
+                34: 2,
+                56: 3
+            },
+            recording: 1
+        },
+        recordBtn: 17,
+        mcps: [ // TODO: Add real mcp addresses
+            {
+                offset: 100,
+                address: 32
+            },
+            {
+                offset: 200,
+                address: 64
+            },
+            {
+                offset: 300,
+                address: 96
+            }
+        ]
     }
 };
